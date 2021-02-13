405e0c9e06f6e1038e9e1b8462ec47ace1c73336
Glitch (nice-economic-actress) <none>
2021-02-13 12:29:05 +0000

    🎌🌮 Checkpoint ./commands/fun/advice.js:27740246/337 ./commands/fun/kiss.js:27740246/1898

---
+++ commands/fun/advice.js
@@ -0,0 +1,27 @@
+const { Random } = require("something-random-on-discord")
+
+const random = new Random();
+
+module.exports = {
+
+  name: "advice",
+  
+  aliases: [""],
+
+  category: "fun",
+
+  description: "Get some advice",
+
+  run: async (client, message, args) => {
+
+    
+
+    let data = await random.getAdvice()
+
+    message.channel.send(data)
+
+    
+
+  }
+
+}
